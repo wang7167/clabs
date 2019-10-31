be83f96d6e9ce623867cd70ed2e7aecceb931086
wang7167 <2503036782@qq.com>
2019-10-31 08:01:40 -0700

    added a ctof.c file

---
+++ lab1/ctof.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+main()
+
+{
+
+    int fahr,celsius;
+
+    int lower,upper,step;
+
+    lower=0;
+
+
+
+    upper=100;
+
+    step=5;
+
+
+
+    celsius=lower;
+
+    printf("celsius--fahr\n");
+
+    printf("\n");
+
+
+
+    while(celsius<=upper){;
+
+         fahr=32+(celsius*1.8);
+
+         printf("%d\t%d\r\n",celsius,fahr);
+
+         celsius=celsius+step;
+
+         }
+
+    }
