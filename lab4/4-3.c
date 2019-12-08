73edad2a266ff33fdfc91b7d18efe412b476361b
wang7167 <2503036782@qq.com>
2019-12-08 06:19:09 -0800

    added a lab4 file

--- lab4/4-3.c
+++ lab4/4-3.c
@@ -40,6 +40,7 @@ int main()
         case '\n':
             printf("\t%.8gg\n",pop());
             break;
+        case's':
         case '%':
             op2 = pop();
             if(op2 != 0.0)
@@ -53,4 +54,70 @@ int main()
         }
     }
     return 0;
+}
+
+#define MAXVAL 100
+
+int sp = 0;
+double val[MAXVAL];
+
+void push(double f)
+{
+    if (sp < MAXVAL)
+    val[sp++] = f;
+    else
+    printf("error: stack full, can't push %g\n",f);
+    
+}
+
+double pop(void)
+{
+    if (sp > 0)
+    return val[--sp];
+    else{
+        printf("error: stack empty\n");
+        return 0.0;
+    }
+}
+
+#include <ctype.h>
+
+int getch(void);
+void ungetch(int);
+
+int getop(char s[])
+{
+    int i, c;
+
+    while ((s[0] = c = getch()) == ' ' || c == '\t')
+    ;
+    s[1] = '\0';
+    if (!isdigit(c) && c != '.')
+    return c;
+    i = 0;
+    if (isdigit(c))
+    while (isdigit(s[++i] = c = getch()))
+    ;
+    s[i] = '\0';
+    if (c != EOF)
+    ungetch(c);
+    return NUMBER;
+    }
+
+#define  BUFSIZE 100
+
+char buf[BUFSIZE];
+int bufp = 0;
+
+int getch(void)
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+    if (bufp >= BUFSIZE)
+    printf("ungetch: too many characters\n");
+    else
+    buf[bufp++] = c;
 }
